a9b0970f99f579ca25b0fa763e413ed8d5ca3b52
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-02-15 13:44:25 +0300

    PMM-1729 Use kingpin for flags.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 
@@ -9,6 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/percona/rds_exporter/basic"
 	"github.com/percona/rds_exporter/config"
@@ -17,10 +17,10 @@ import (
 )
 
 var (
-	listenAddress       = flag.String("web.listen-address", ":9042", "Address on which to expose metrics and web interface.")
-	basicMetricsPath    = flag.String("web.basic-telemetry-path", "/basic", "Path under which to expose exporter's basic metrics.")
-	enhancedMetricsPath = flag.String("web.enhanced-telemetry-path", "/enhanced", "Path under which to expose exporter's enhanced metrics.")
-	configFile          = flag.String("config.file", "config.yml", "Path to configuration file.")
+	listenAddress       = kingpin.Flag("web.listen-address", "Address on which to expose metrics and web interface.").Default(":9042").String()
+	basicMetricsPath    = kingpin.Flag("web.basic-telemetry-path", "Path under which to expose exporter's basic metrics.").Default("/basic").String()
+	enhancedMetricsPath = kingpin.Flag("web.enhanced-telemetry-path", "Path under which to expose exporter's enhanced metrics.").Default("/enhanced").String()
+	configFile          = kingpin.Flag("config.file", "Path to configuration file.").Default("config.yml").String()
 
 	settings     *config.Settings
 	sessionsPool *sessions.Sessions
@@ -38,10 +38,11 @@ func handleReload(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	log.AddFlags(kingpin.CommandLine)
 	log.Infoln("Starting RDS exporter", version.Info())
 	log.Infoln("Build context", version.BuildContext())
 
-	flag.Parse()
+	kingpin.Parse()
 
 	// Create settings.
 	settings = &config.Settings{}
