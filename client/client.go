70eb780d72194516fcd0a3b35deb1fde5b0df99c
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2018-10-01 17:31:41 +0300

    PMM-2116 Add documentation comments.

--- client/client.go
+++ client/client.go
@@ -7,11 +7,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// Client represents HTTP client for all AWS APIs with metrics reporting.
 type Client struct {
 	c *http.Client
 	t *transport
 }
 
+// New creates new Client.
 func New() *Client {
 	t := newTransport()
 	return &Client{
@@ -23,14 +25,17 @@ func New() *Client {
 	}
 }
 
+// HTTP returns underlying *http.Client.
 func (c *Client) HTTP() *http.Client {
 	return c.c
 }
 
+// Describe implements prometheus.Collector.
 func (c *Client) Describe(ch chan<- *prometheus.Desc) {
 	c.t.mRequests.Describe(ch)
 }
 
+// Collect implements prometheus.Collector.
 func (c *Client) Collect(ch chan<- prometheus.Metric) {
 	c.t.mRequests.Collect(ch)
 }
